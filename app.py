a7ccba15ae9533132e79ddab3384ae1b921263ff
yingMC1 <ying110189@outlook.com>
2026-07-16 09:53:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -279,7 +279,7 @@ def handle_admin_message(data):
         emit('system_message', {'content': f'[管理员] {content}'}, room=room)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
